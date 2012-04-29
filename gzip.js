d7b770dccbe4b74534fa40f69173a7a23e864c13
David Björklund <david.bjorklund@gmail.com>
2012-04-30 00:15:33 +0200

    Set headers correctly

--- gzip.js
+++ gzip.js
@@ -7,8 +7,6 @@ module.exports = function(req, res) {
   var negotiator = new Negotiator(req);
 
   if (negotiator.preferredEncodings().indexOf('gzip') !== -1) {
-    res.setHeader('content-encoding', 'gzip');
-
     var gzip = zlib.Gzip()
       , _write = res._write
       , _end = res._end
@@ -26,6 +24,9 @@ module.exports = function(req, res) {
     res._write = function(data) {
       hasData = true;
       gzip.write(data);
+      if (res._header) return; // Don't write when header is finished
+      res.setHeader('content-encoding', 'gzip');
+      res.removeHeader('content-length');
     }
 
     res._end = function() {
